6708663297311a7ebbf8950bd33bb1170a93ee01
Diankrs30 <diankrs30@gmail.com>
2022-10-10 17:58:15 +0700

    env database

--- src/config/postgre.js
+++ src/config/postgre.js
@@ -2,14 +2,15 @@
 // koneksi pg yang ada didepedencies
 // const pg = require("pg");
 // const { Pool } = pg; atau
+require("dotenv").config();
 
 const { Pool } = require("pg");
 const db = new Pool({
-  host: "localhost",
-  user: "dian",
-  database: "dian_coffeshop",
-  password: "qwerty",
-  port: "5432",
+  host: process.env.HOST,
+  user: process.env.USER,
+  database: process.env.DATABASE,
+  password: process.env.PASSWORD,
+  port: process.env.PORT,
 });
 
 module.exports = db;
