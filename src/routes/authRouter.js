abb6ab5373da9b0038e04bab5ef90d2aff5dd842
Diankrs30 <diankrs30@gmail.com>
2022-10-19 03:18:33 +0700

    add logout

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const authRouter = express.Router();
 const validate = require("../middleware/validate");
-const { login } = require("../controllers/auth");
+const { login, logout } = require("../controllers/auth");
 
 authRouter.post("/login", validate.loginBody, login);
-// authRouter.delete("/", logout);
+authRouter.delete("/logout", logout);
 
 // export router
 module.exports = authRouter;
