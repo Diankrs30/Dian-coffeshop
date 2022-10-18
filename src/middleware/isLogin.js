8ba78a55aa846836ea49707e7c5a1fd3eb4e221f
Diankrs30 <diankrs30@gmail.com>
2022-10-19 03:23:03 +0700

    add validasi token

--- src/middleware/isLogin.js
+++ src/middleware/isLogin.js
@@ -1,34 +1,43 @@
 const jwt = require("jsonwebtoken");
 const response = require("../helper/response");
+const usersRepo = require("../repo/usersRepo");
 
-module.exports = () => {
-  return (req, res, next) => {
-    const token = req.header("x-access-token");
-    if (!token)
-      return response(res, {
-        status: 401,
-        message: "You have to login first",
-        data: null,
-      });
+const isLogin = async (req, res, next) => {
+  const token = req.header("x-access-token");
+  if (!token)
+    return response(res, {
+      status: 401,
+      message: "You have to login first",
+      data: null,
+    });
+  const checkWhitelistToken = await usersRepo.checkWhitelistToken(token);
+  // console.log(checkWhitelistToken.rows.length);
+  if (checkWhitelistToken.rows.length === 0) {
+    return response(res, {
+      status: 400,
+      message: "You have to login first",
+    });
+  }
 
-    // verifikasi
-    jwt.verify(
-      token,
-      process.env.SECRET_KEY,
-      { issuer: process.env.ISSUER },
-      (error, decodedPayload) => {
-        if (error) {
-          console.log(error);
-          return response(res, {
-            status: 403,
-            message: "Authentication failed",
-            error: error.message,
-          });
-        }
-        // payload
-        req.userPayload = decodedPayload;
-        next();
+  // verifikasi
+  jwt.verify(
+    token,
+    process.env.SECRET_KEY,
+    { issuer: process.env.ISSUER },
+    (error, decodedPayload) => {
+      if (error) {
+        console.log(error);
+        return response(res, {
+          status: 403,
+          message: "Authentication failed",
+          error: error.message,
+        });
       }
-    );
-  };
+      // payload
+      req.userPayload = decodedPayload;
+      next();
+    }
+  );
 };
+
+module.exports = { isLogin };
